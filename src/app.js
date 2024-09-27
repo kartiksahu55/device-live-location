46bab169b45932b6bb88fbcf4590cb9d159efb2b
kartiksahu55 <kartiksahu55@gmail.com>
2024-09-27 14:42:13 +0530

    updated app.js to fix the error

--- src/app.js
+++ src/app.js
@@ -18,8 +18,9 @@ io.on("connection", (socket) => {
   console.log("connected");
 });
 
-app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
