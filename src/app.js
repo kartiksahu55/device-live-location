8c317dcf8230584bb3f512698fd0293d27133500
kartiksahu55 <kartiksahu55@gmail.com>
2024-09-27 14:28:52 +0530

    Implemented index.js setup

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import { Server } from "socket.io";
 import { createServer } from "http";
-import path from "path";
 import { fileURLToPath } from "url";
+import path from "path";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello Kartik");
