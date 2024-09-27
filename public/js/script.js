6e202cce73989e305aa8b1546793e674c14902be
kartiksahu55 <kartiksahu55@gmail.com>
2024-09-27 17:53:08 +0530

    Updated js/script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,44 @@
-console.log("Inside script");
+const socket = io();
 
-const socket = io()
-console.log(socket);
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition((position) => {
+    const { latitude, longitude } = position.coords;
+
+    socket.emit("send-location", { latitude, longitude });
+  }),
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maxAge: 0,
+    };
+}
+
+const map = L.map("map").setView([0, 0], 1);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Kartik (Frontend Developer)",
+}).addTo(map);
+
+const marker = {};
+
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+
+  map.setView([latitude, longitude], 16);
+
+  if (marker[id]) {
+    marker[id].setlatLng([latitude, longitude]);
+  } else {
+    marker[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (marker[id]) {
+    map.removeLayer(marker[id]);
+    delete marker[id];
+  }
+});
